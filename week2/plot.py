fb2e5df4b5e2b37cd034294d04b2499423422e31
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-09-10 18:56:03 -0400

    made my own plot.py to make sure I could do it

--- week2/plot.py
+++ week2/plot.py
@@ -1,53 +1,57 @@
-# install matplotlib using the python package manager: pip
-# pip install matplotlib
+# Gabriel Malone / CS205 / Week 2 Lab
 
 import os
-import sys						# command line arguments
-from matplotlib import pyplot 	# plotting functions
-from glob import glob			# pathname pattern expansion module
-								# use to only read text files from current directory
+import matplotlib.pyplot as plt
 
+# dictionary to hold graph data 
+# key will be experiment name
+# value will be x, y lists of values
+experiments = {}
 
-def prepare_file_name(file_name: str) -> str:
-	tokens = file_name[:-4].split("_")
-	for i in range(len(tokens)):
-		tokens[i] = tokens[i].capitalize()
-	return ' '.join(tokens)
+# format names to standard required by lab instructions
+# (remove '_', ap each word)
+def cleanName(file_name:str)->str:  
+    caps = []  
+    
+    tokens = file_name[0:-4].split('_')
+    for token in tokens:
+        caps.append(token.capitalize())
+    clean_name = " ".join(caps)
+    return clean_name
 
-# list comprehension to read only text file names into list
-# looks in current directory
-# use glob module to pattern match files names by type
-files = [f for f in glob('*.txt') if "CMake" not in f]
+def openTextFile(file_name:str)->None:
+    x_data = [] # this list will be a value for the dictionary 'experiments'
+    y_data = [] # this list will be a value for the dictionary 'experiments'
+    experiments[cleanName(file_name)] = x_data, y_data  
+    with open(file_name) as f:
+         # get each trial number (x coord) 
+         # and its corresponding n-value (y coord)
+        for line in f:
+            line = line.strip()
+            tokens = line.split(" ")
+            # add data to their corresponding lists
+            # convert to int otherwise a big mess in matplot
+            x_data.append(int(tokens[0]))
+            y_data.append(int(tokens[1]))
 
-# load data from file names
-plots = {}										# dictionary of plots
-for file_name in files:							# iterate through file names
-	file = open(file_name)						# open file
-	x = []										# list of x values - number of trials
-	y = []										# list of y values - time complexity
-	z = []										# data point values  
-	for line in file:							# iterate through file
-		line = line.strip().split(" ")			# clean up the data
-		x.append(int(line[0]))					# build x list
-		y.append(int(line[1]))					# build y list
-		z.append(int(line[2]))					# build z list
-		plots[prepare_file_name(file_name)] = (x, y, z) # prepare graph legend from file name and associate x, y, z lists
+# get text files
+text_files = [file for file in os.listdir() if '.txt' in file]
 
-# plot each set
-for s in plots:
-	pyplot.plot(plots[s][0], plots[s][1], label=s)
-	labels = plots[s][2]
-	
-pyplot.legend()					# show legend
-if len(sys.argv) > 1:
-	pyplot.title(sys.argv[1])	# display title from command line args
-else:
-	pyplot.title("Time Complexity Graph")	# default title
-pyplot.xlabel("Problem Size")	# axis label
-pyplot.ylabel("Comparisons")	# axis label
-pyplot.grid(True)				# show the grid
-pyplot.show()					# show the plot
+# load the data from text files above
+for file in text_files:
+    openTextFile(file)
 
-# clean up txt files after done
-for file in files:
+# plot the data via matplotlib
+for experiment in experiments:
+    # get x data from dictionary key (experiment) values index 0, y data from dictionary values index 1
+    plt.plot(experiments[experiment][0],experiments[experiment][1], scalex=True, scaley=True, linewidth=1.0, aa=True)
+plt.title("Time Complexity Graph")
+plt.legend(experiments)
+plt.xlabel("Number of Trials")
+plt.ylabel("N-Value Computations")
+plt.grid(True)
+plt.show()
+
+# remove text data from folder when done (for multiple runs of program)
+for file in text_files:
 	os.remove(file)
