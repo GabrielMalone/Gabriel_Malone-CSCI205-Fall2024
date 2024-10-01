153bef6f8fc7e200a554c78567e28be0a7b2551d
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-10-01 15:13:47 -0400

    hanoi print upgrades

--- week6/Hanoi/towers_main.cpp
+++ week6/Hanoi/towers_main.cpp
@@ -4,14 +4,18 @@
 #include <iomanip>
 #include "Disc.hpp"
 #include "../backend/List.hpp"
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 List<Disc>tower_A;
 List<Disc>tower_B;
 List<Disc>tower_C;
-
+size_t tower_depth;
 int counter = 0;
+string CLEAR_SCREEN  = "\033[2J";  // Clears the screen
+string CURSOR_TOP_LEFT = "\033[H";
 
 void initializeGame(size_t);
 void printTowers();
@@ -25,6 +29,7 @@ int main(){
     string towersize;
     cout << "Welcome to the Towers of Hanoi\nPlease enter size of tower: ";
     cin >> towersize;
+    tower_depth = stoi(towersize);
     initializeGame(stoi(towersize) * 2);
     printTowers();
     char whatever;
@@ -35,10 +40,15 @@ int main(){
 }
 
 void moveDisk(List<Disc>& fp, List<Disc>& tp){
-    cout <<"\nMOVE NUMBER: " << counter << " Moving disk "; fp.get_head_element().print_disc(); cout << "from " << fp.getName() << " to " << tp.getName() << "\n" << endl;
+    cout << CLEAR_SCREEN;    // Clear the screen
+    cout << CURSOR_TOP_LEFT;
+    cout << "-----------------------------------------------------------------------------------------------------------------------------------------" << endl;
+    cout <<"MOVE NUMBER: " << counter << " Moving disk "; fp.get_head_element().print_disc(); cout << "from " << fp.getName() << " to " << tp.getName() << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------------------------------" << endl;
     Disc top = fp.remove(0);
     tp.insert(top);
     printTowers();
+
 }
 
 void towers_of_hanoi(int height, List<Disc>& fromPole, List<Disc>& toPole, List<Disc>& withPole){
@@ -71,25 +81,20 @@ void initializeGame(size_t towerHeight){
  */
 void printTowers(){
     // get heighest stack to iterate from
-    Node<Disc>* tallest;                            // this will guide when each tower can start printing their discs
     Node<Disc>* a = tower_A.get_head();             // get the head of each tower so you can iterate through the discs
     Node<Disc>* b = tower_B.get_head();
     Node<Disc>* c = tower_C.get_head();
     int height = 0;                                 // this will serve as a way to track what height of the tower the print function is at
-    if (tower_A.length() > height){                 // now let's find which tower is the tallest
+    if ((int)tower_A.length() > height){                 // now let's find which tower is the tallest
         height = tower_A.length();
-        tallest = tower_A.get_head(); 
       
     }
-    if (tower_B.length() > height){
+    if ((int)tower_B.length() > height){
         height = tower_B.length();
-        tallest = tower_B.get_head();
       
     }
-    if (tower_C.length() > height){
+    if ((int)tower_C.length() > height){
         height = tower_C.length();
-        tallest = tower_C.get_head();
-   
     }                                              // start printing from the tallest tower's height
     int start_print_heightA = tower_A.length();    // these will be used to see when each tower can start printing their discs
     int start_print_heightB = tower_B.length();
@@ -98,7 +103,7 @@ void printTowers(){
     bool B_can_print = false;
     bool C_can_print = false;                   
     cout << "                   tower a                                      tower b                                        tower c\n" << endl;
-    while (tallest != NULL){                        // iterate for duration of tallest tower
+    for (int i = 0 ; i < tower_depth ; i ++){                        // iterate for duration of tallest tower
         if (start_print_heightA  - height == 0){    // once these trigger, that tower can start printing
             A_can_print = true;
         }
@@ -108,28 +113,29 @@ void printTowers(){
         if (start_print_heightC -  height == 0 ){
             C_can_print = true;
         }
-        if (a != NULL && A_can_print){           
+        if (a != NULL && A_can_print && tower_A.length() >= tower_depth - i){           
             a->data.print_disc();
             a = a->next;
          
         } else {
             cout << "[                                           ] ";    // if null or cant print, place a blank
         }
-        if (b != NULL && B_can_print){
+        if (b != NULL && B_can_print && tower_B.length() >= tower_depth - i){
             b->data.print_disc();
             b = b->next;
          
         } else {
             cout << "[                                           ] ";
         }
-        if (c != NULL && C_can_print){
+        if (c != NULL && C_can_print && tower_C.length() >= tower_depth - i){
             c->data.print_disc();
             c = c->next;
         } else {
             cout << "[                                           ] ";
         }
-        tallest = tallest->next;
+        //tallest = tallest->next;
         cout << endl;
         height -- ;
     }
+    this_thread::sleep_for(chrono::milliseconds(100));
 }
