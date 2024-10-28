980273b82ec9a8b0e15933abfd7698144945f27b
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-10-28 08:50:49 -0400

    need to make sure the data folder is present

--- week10/sorting_methods/helper_methods/median_of_three.hpp
+++ week10/sorting_methods/helper_methods/median_of_three.hpp
@@ -7,12 +7,12 @@ using namespace std;
 
 int MO3(vector<int>& v, int left, int right){
     int center = (left + right) / 2;	// find center
-	swap(v[left], v[center]);			// get ready to sort the three
+	//swap(v[left], v[center]);			// get ready to sort the three
 	if( v[left] > v[right] )			// look at left and right
 		swap(v[left], v[right]);		// swap if necessary
 	if(v[center] > v[right] )			// look at center and right
 		swap(v[center], v[right]);		// swap if necessary
-	swap(v[center], v[right]);			// put pivot on right
+	//swap(v[center], v[right]);			// put pivot on right
 	return v[right];					// return pivot value
 }
 
